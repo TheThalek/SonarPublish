0367b06f2578c8e37b9455170230b49ceaa14152
Thale <finkthale@gmail.com>
2024-03-21 13:46:28 +0100

    Fixed more of georeferencing

--- Subscriber/Georeferencing/Georeferencing.py
+++ Subscriber/Georeferencing/Georeferencing.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from Classes import SonarData, TelemetryDataPosition, TelemetryDataPose, TelemetryDataHeading, TelemetryDataDepth, TelemetryDataAltitude
 from Visualization import visualize_all_rotations
-
+from Math import llh2ecef
 
 # Function to load data from JSON file
 def load_data_from_json(filename):
@@ -32,6 +32,11 @@ if __name__ == "__main__":
 
     # Lists for saving data from loop 
     all_R_BN = []
+    all_P_N = []
+    all_T_N = []
+    # R_BN_first = 0
+    # R_BN_second = 0
+
 
     for message in data:
         sonar, position, pose, heading, depth, altitude = process_message(message)
@@ -46,43 +51,62 @@ if __name__ == "__main__":
             #  TO DO; Visualize these points, to see if P_S and P_B has a correct offset wrt. each other
         
         # Body to NED transformation
+        roll = math.radians(pose.roll)
+        pitch = math.radians(pose.pitch)
+        heading = math.radians(heading.heading)
         R_X = np.array([
             [1, 0, 0],
-            [0, math.cos(pose.roll), -math.sin(pose.roll)],
-            [0, math.sin(pose.roll), math.cos(pose.roll)]
+            [0, math.cos(roll), -math.sin(roll)],
+            [0, math.sin(roll), math.cos(roll)]
         ])
 
         R_Y = np.array([
-            [math.cos(pose.pitch), 0, math.sin(pose.pitch)],
+            [math.cos(pitch), 0, math.sin(pitch)],
             [0, 1, 0],
-            [-math.sin(pose.pitch), 0, math.cos(pose.pitch)]
+            [-math.sin(pitch), 0, math.cos(pitch)]
         ])
 
         R_Z = np.array([
-            [math.cos(heading.heading), -math.sin(heading.heading), 0],
-            [math.sin(heading.heading), math.cos(heading.heading), 0],
+            [math.cos(heading), -math.sin(heading), 0],
+            [math.sin(heading), math.cos(heading), 0],
             [0, 0, 1]
         ])
 
         R_BN = R_Z @ R_Y @ R_X # Rotation matrix from body to NED
             # TO DO; Visualize this rotation matrix, to see if the rotation makes sense
         all_R_BN.append(R_BN) # For visualization later
+        R_BN_second = R_BN_first
+        R_BN_first = R_BN
+
+        total_change += R_BN
+        print("Difference in rotation", R_BN_second-R_BN_first)
+        
 
         # Calculate the transformed points
         P_N = R_BN @ P_B  # Assuming P_B is correctly shaped for matrix multiplication
-
+        # print(P_N)
         # If P_B represents multiple points, make sure it's of shape (3, N)
         # And your T_N should be broadcastable to the shape of P_N
-        T_N = np.array([0, 0, 0])
-        T_N = np.reshape(T_N, (3, 1))  # Reshape for broadcasting
 
-        P_N += T_N  # Add T_N to each column of P_N
-            # TO DO; Visualize these points, to see that they follow the rotation matrix as well
-    
+        longitude = math.radians(position.longitude)
+        latitude = math.radians(position.latitude)
+        # print("long", longitude, "lat", latitude, "\n")
+        x_ecef, y_ecef, z_ecef = llh2ecef(longitude, latitude, -depth.depth)
+        # print("x_ecef", x_ecef, "y_ecef", y_ecef, "z_ecef", z_ecef, "\n")
+
+
+
+        # T_N = np.array([x_ecef, y_ecef, z_ecef])
+        # T_N = np.reshape(T_N, (3, 1))  # Reshape for broadcasting
+        # all_T_N.append(T_N)  # Save T_N for each message
+
+        # P_N += T_N  # Add T_N to each column of P_N
+        # all_P_N.append(P_N)
 
     visualize_all_rotations(all_R_BN)
 
 
+    
 
 
         
--- Subscriber/Georeferencing/Visualization.py
+++ Subscriber/Georeferencing/Visualization.py
@@ -1,63 +1,94 @@
 import open3d as o3d
 import numpy as np
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def visualize_data(all_R_BN, all_P_N, all_T_N):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plotting all the points (P_N)
+    for P_N in all_P_N:
+        ax.scatter(P_N[0,:], P_N[1,:], P_N[2,:], marker='.')
+
+    # Visualizing rotation matrices
+    for R_BN, T_N in zip(all_R_BN, all_T_N):
+        # Origin of the rotation matrix in ECEF frame
+        origin = T_N.flatten()
+        # Unit vectors for axes
+        axes = np.identity(3)
+        
+        # Transforming axes according to the rotation matrix and translating
+        transformed_axes = R_BN @ axes + origin[:, np.newaxis]
+        
+        # Drawing axes
+        for i, color in zip(range(3), ['r', 'g', 'b']): # RGB for XYZ axes
+            ax.quiver(*origin, *(transformed_axes[:,i] - origin), length=0.1, color=color)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+
+
 def visualize_all_rotations(rotations, spacing=1):
-    mesh = mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    # mesh = mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    """
+    Visualizes all rotations with automatic x-axis offsets in Open3D.
+
+    :param rotations: List of rotation matrices (R_BN).
+    :param spacing: Distance between consecutive rotations along the x-axis.
+    """
+    # Create an Open3D point cloud to visualize the axes points
+    pcd = o3d.geometry.PointCloud()
+
+    # Define axis points in their default orientation
+    axis_points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    # Create a list to hold all points for visualization
+    all_points = []
+
+    for i, R_BN in enumerate(rotations):
+        # Generate offset based on index to separate each set of axes visually
+        offset = np.array([i * spacing, 0, 0])
+
+        # Apply rotation and add offset to each of the axis points
+        rotated_points = np.dot(R_BN, axis_points.T).T + offset
+        all_points.extend(rotated_points)
+
+    # Convert all points into a format suitable for Open3D
+    pcd.points = o3d.utility.Vector3dVector(all_points)
+
+    # Create lines to represent the axes
+    # Lines are defined by pairs of indices into the points list
+    lines = []
+    colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]  # Colors for the X, Y, Z axes
+    line_colors = []
+
+    for i in range(len(rotations)):
+        base_index = i * 4  # Starting index for each set of axes points
+        lines.append([base_index, base_index + 1])  # X-axis
+        lines.append([base_index, base_index + 2])  # Y-axis
+        lines.append([base_index, base_index + 3])  # Z-axis
+        line_colors.extend(colors)
+
+    # Create an Open3D line set for the axes lines
+    line_set = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector(all_points),
+        lines=o3d.utility.Vector2iVector(lines),
+    )
+    line_set.colors = o3d.utility.Vector3dVector(line_colors)
+
+    # Initialize an Open3D visualizer
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    vis.add_geometry(line_set)
+    vis.run()
+    vis.destroy_window()
 
 
 
 
-
-    # """
-    # Visualizes all rotations with automatic x-axis offsets in Open3D.
-
-    # :param rotations: List of rotation matrices (R_BN).
-    # :param spacing: Distance between consecutive rotations along the x-axis.
-    # """
-    # # Create an Open3D point cloud to visualize the axes points
-    # pcd = o3d.geometry.PointCloud()
-
-    # # Define axis points in their default orientation
-    # axis_points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
-
-    # # Create a list to hold all points for visualization
-    # all_points = []
-
-    # for i, R_BN in enumerate(rotations):
-    #     # Generate offset based on index to separate each set of axes visually
-    #     offset = np.array([i * spacing, 0, 0])
-
-    #     # Apply rotation and add offset to each of the axis points
-    #     rotated_points = np.dot(R_BN, axis_points.T).T + offset
-    #     all_points.extend(rotated_points)
-
-    # # Convert all points into a format suitable for Open3D
-    # pcd.points = o3d.utility.Vector3dVector(all_points)
-
-    # # Create lines to represent the axes
-    # # Lines are defined by pairs of indices into the points list
-    # lines = []
-    # colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]  # Colors for the X, Y, Z axes
-    # line_colors = []
-
-    # for i in range(len(rotations)):
-    #     base_index = i * 4  # Starting index for each set of axes points
-    #     lines.append([base_index, base_index + 1])  # X-axis
-    #     lines.append([base_index, base_index + 2])  # Y-axis
-    #     lines.append([base_index, base_index + 3])  # Z-axis
-    #     line_colors.extend(colors)
-
-    # # Create an Open3D line set for the axes lines
-    # line_set = o3d.geometry.LineSet(
-    #     points=o3d.utility.Vector3dVector(all_points),
-    #     lines=o3d.utility.Vector2iVector(lines),
-    # )
-    # line_set.colors = o3d.utility.Vector3dVector(line_colors)
-
-    # # Initialize an Open3D visualizer
-    # vis = o3d.visualization.Visualizer()
-    # vis.create_window()
-    # vis.add_geometry(pcd)
-    # vis.add_geometry(line_set)
-    # vis.run()
-    # vis.destroy_window()
